96af87b0a085545f4e0d3d1e9870146917120387
dinhquang2909 <33979046+dinhquang2909@users.noreply.github.com>
2017-11-25 18:23:14 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://http://luckylandcorp.com/wp-content/uploads/2016/06/1-LL-Group-Ph%E1%BB%91i-c%E1%BA%A3nh-t%E1%BB%95ng-th%E1%BB%83-d%E1%BB%B1-%C3%A1n-C%E1%BB%99ng-Ho%C3%A0-Garden.jpg' width='500px'/><br/>bán căn hộ Cộng Hòa Garden</a>. Liên Hệ: 0911397444. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://luckylandcorp.com/wp-content/uploads/2016/06/1-LL-Group-Ph%E1%BB%91i-c%E1%BA%A3nh-t%E1%BB%95ng-th%E1%BB%83-d%E1%BB%B1-%C3%A1n-C%E1%BB%99ng-Ho%C3%A0-Garden.jpg' width='500px'/><br/>bán căn hộ Cộng Hòa Garden</a>. Liên Hệ: 0911397444. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
